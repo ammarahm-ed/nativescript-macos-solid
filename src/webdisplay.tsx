b8c537c7c847d6ecae57fb24e51153d4911b7ad5
Nathan Walker <walkerrunpdx@gmail.com>
2024-10-21 11:29:06 -0700

    feat: webdisplay loading state

--- src/webdisplay.tsx
+++ src/webdisplay.tsx
@@ -1,15 +1,33 @@
-import type { Component } from "npm:solid-js";
+import { Show, type Component, createSignal } from "npm:solid-js";
 
 interface WebDisplayProps {
   url: string;
 }
 
 const WebDisplay: Component<WebDisplayProps> = (props) => {
+  const [isLoading, setIsLoading] = createSignal<boolean>(false);
   return (
-    <webview
-      src={props.url}
-      style={{ width: "100%", height: "100%" }}
-    ></webview>
+    <view style={{ width: "100%", height: "100%" }}>
+      <Show when={isLoading()}>
+        <progress
+          style={{
+            width: "100%",
+            height: 20,
+          }}
+          indeterminate={true}
+        />
+      </Show>
+      <webview
+        src={props.url}
+        style={{ width: "100%", height: "100%" }}
+        onLoadStarted={() => {
+          setIsLoading(true);
+        }}
+        onLoadFinished={() => {
+          setIsLoading(false);
+        }}
+      ></webview>
+    </view>
   );
 };
 
