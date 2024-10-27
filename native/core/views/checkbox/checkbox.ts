c88e3fe09d676c6bf192715ade0ffeff84f97be8
Ammar Ahmed <ammarahmed6506@gmail.com>
2024-10-27 11:54:26 +0500

    support checked/unchecked state in checkbox

--- native/core/views/checkbox/checkbox.ts
+++ native/core/views/checkbox/checkbox.ts
@@ -1,6 +1,7 @@
-import { view } from "../decorators/view.ts";
-import { NativeButton } from "../button/native-button.ts";
 import { Button } from "../button/button.ts";
+import { NativeButton } from "../button/native-button.ts";
+import { native } from "../decorators/native.ts";
+import { view } from "../decorators/view.ts";
 
 @view({
   name: "HTMLButtonElement",
@@ -10,5 +11,21 @@ export class Checkbox extends Button {
   public override prepareNativeView(nativeView: NativeButton): void {
     super.prepareNativeView(nativeView);
     nativeView.setButtonType(NSButtonType.Switch);
+    nativeView.title = "";
   }
+
+  public isChecked(): boolean {
+    return this.nativeView?.state === NSControlStateValueOn;
+  }
+
+  @native({
+    setNative: (view: Checkbox, key, value) => {
+      if (view.nativeView) {
+        view.nativeView.state = value
+          ? NSControlStateValueOn
+          : NSControlStateValueOff;
+      }
+    },
+  })
+  declare checked: boolean;
 }
